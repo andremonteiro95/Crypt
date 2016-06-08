fdd25518a36ef80a6b7369f70cfa7690119aac0c
André Monteiro <andremonteiro-@hotmail.com>
2016-06-08 17:08:59 +0100

    Init branch

---
+++ app/src/main/java/pt/ubi/andremonteiro/crypt/dropboxutils/MeoCloudFragment.java
@@ -0,0 +1,57 @@
+package pt.ubi.andremonteiro.crypt.dropboxutils;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+
+import pt.ubi.andremonteiro.crypt.R;
+
+public class MeoCloudFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public MeoCloudFragment() {
+        // Required empty public constructor
+    }
+
+    public static MeoCloudFragment newInstance(String param1, String param2) {
+        MeoCloudFragment fragment = new MeoCloudFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meocloud, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) { super.onAttach(context); }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
